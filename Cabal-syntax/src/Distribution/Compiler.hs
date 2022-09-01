3d2f511a49e5f80cf83f120593ee7808fedfccd1
Andrea Bedini <andrea@andreabedini.com>
2022-09-01 12:47:29 +0200

    refactor: remove support for LHC

--- Cabal-syntax/src/Distribution/Compiler.hs
+++ Cabal-syntax/src/Distribution/Compiler.hs
@@ -72,7 +72,6 @@ data CompilerFlavor
   | HBC
   | Helium
   | JHC
-  | LHC
   | Eta
   | -- | @since 3.12.1.0
     -- MicroHS, see https://github.com/augustss/MicroHs
@@ -86,7 +85,7 @@ instance NFData CompilerFlavor where rnf = genericRnf
 
 knownCompilerFlavors :: [CompilerFlavor]
 knownCompilerFlavors =
-  [GHC, GHCJS, NHC, YHC, Hugs, HBC, Helium, JHC, LHC, Eta, MHS]
+  [GHC, GHCJS, NHC, YHC, Hugs, HBC, Helium, JHC, Eta, MHS]
 
 instance Pretty CompilerFlavor where
   pretty (OtherCompiler name) = Disp.text name
