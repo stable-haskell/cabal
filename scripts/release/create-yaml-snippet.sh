b4734b308558f52cbe04ffe775199bcfee4df6de
Julian Ospald <hasufell@posteo.de>
2023-11-13 17:40:07 +0800

    Migrate release CI back to github

---
+++ scripts/release/create-yaml-snippet.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+RELEASE=$1
+VERSION=${RELEASE#cabal-install-v}
+
+cd "gh-release-artifacts/cabal-${VERSION}"
+BASE_URL=https://downloads.haskell.org/~ghcup/unofficial-bindists/cabal/$VERSION
+
+cat <<EOF > /dev/stdout
+    $VERSION:
+      viTags:
+        - Latest
+      viChangeLog: https://github.com/haskell/cabal/blob/master/release-notes/cabal-install-$VERSION.md
+      viArch:
+        A_64:
+          Linux_Debian:
+            '(>= 10 && < 11)': &cabal-${VERSION//./}-64-deb10
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-deb10.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-deb10.tar.xz" | awk '{ print $1 }')
+            '(>= 11 && < 12)': &cabal-${VERSION//./}-64-deb11
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-deb11.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-deb11.tar.xz" | awk '{ print $1 }')
+            '(>= 12 && < 13)': &cabal-${VERSION//./}-64-deb12
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-deb12.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-deb12.tar.xz" | awk '{ print $1 }')
+            unknown_versioning: *cabal-${VERSION//./}-64-deb12
+          Linux_Ubuntu:
+            '( >= 20 && < 22 )': &cabal-${VERSION//./}-64-ubuntu20
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-ubuntu20.04.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-ubuntu20.04.tar.xz" | awk '{ print $1 }')
+            '( >= 22 && < 24 )': &cabal-${VERSION//./}-64-ubuntu22
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-ubuntu22.04.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-ubuntu22.04.tar.xz" | awk '{ print $1 }')
+            '( >= 24 && < 25 )': &cabal-${VERSION//./}-64-ubuntu24
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-ubuntu24.04.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-ubuntu24.04.tar.xz" | awk '{ print $1 }')
+            unknown_versioning: *cabal-${VERSION//./}-64-ubuntu24
+          Linux_Mint:
+            '(>= 20 && < 21)': &cabal-${VERSION//./}-64-mint20
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-mint20.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-mint20.tar.xz" | awk '{ print $1 }')
+            '>= 21': &cabal-${VERSION//./}-64-mint21
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-mint21.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-mint21.tar.xz" | awk '{ print $1 }')
+            unknown_versioning: *cabal-${VERSION//./}-64-mint21
+          Linux_Fedora:
+            '(>= 33 && < 37)': &cabal-${VERSION//./}-64-fedora33
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-fedora33.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-fedora33.tar.xz" | awk '{ print $1 }')
+            '>= 37': &cabal-${VERSION//./}-64-fedora37
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-fedora37.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-fedora37.tar.xz" | awk '{ print $1 }')
+            unknown_versioning: *cabal-${VERSION//./}-64-fedora37
+          Linux_Rocky:
+            '( >= 8 && < 9 )': &cabal-${VERSION//./}-64-rocky8
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-rocky8.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-rocky8.tar.xz" | awk '{ print $1 }')
+            '( >= 9 )': &cabal-${VERSION//./}-64-rocky9
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-rocky9.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-rocky9.tar.xz" | awk '{ print $1 }')
+            unknown_versioning: *cabal-${VERSION//./}-64-rocky9
+          Linux_RedHat:
+            unknown_versioning: *cabal-${VERSION//./}-64-rocky8
+          Linux_UnknownLinux:
+            unknown_versioning: &cabal-${VERSION//./}-64-unknown
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-unknown.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-unknown.tar.xz" | awk '{ print $1 }')
+          Linux_Alpine:
+            '( >= 3.12 && < 3.19 )': &cabal-${VERSION//./}-64-alpine312
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-alpine312.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-alpine312.tar.xz" | awk '{ print $1 }')
+            '( >= 3.19 )': &cabal-${VERSION//./}-64-alpine319
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-linux-alpine319.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-linux-alpine319.tar.xz" | awk '{ print $1 }')
+            unknown_versioning: *cabal-${VERSION//./}-64-alpine319
+          Linux_Void:
+            unknown_versioning: *cabal-${VERSION//./}-64-unknown
+          Darwin:
+            unknown_versioning:
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-apple-darwin.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-apple-darwin.tar.xz" | awk '{ print $1 }')
+          Windows:
+            unknown_versioning:
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-mingw64.zip
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-mingw64.zip" | awk '{ print $1 }')
+          FreeBSD:
+            unknown_versioning:
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-x86_64-portbld-freebsd.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-x86_64-portbld-freebsd.tar.xz" | awk '{ print $1 }')
+        A_32:
+          Linux_UnknownLinux:
+            unknown_versioning: &cabal-${VERSION//./}-32-unknown
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-i386-linux-unknown.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-i386-linux-unknown.tar.xz" | awk '{ print $1 }')
+          Linux_Alpine:
+            unknown_versioning: *cabal-${VERSION//./}-32-unknown
+        A_ARM64:
+          Linux_UnknownLinux:
+            unknown_versioning:
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-aarch64-linux-deb10.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-aarch64-linux-deb10.tar.xz" | awk '{ print $1 }')
+          Linux_Alpine:
+            unknown_versioning:
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-aarch64-linux-alpine.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-aarch64-linux-alpine.tar.xz" | awk '{ print $1 }')
+          Darwin:
+            unknown_versioning:
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-aarch64-apple-darwin.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-aarch64-apple-darwin.tar.xz" | awk '{ print $1 }')
+        A_ARM:
+          Linux_UnknownLinux:
+            unknown_versioning:
+              dlUri: ${BASE_URL}/cabal-install-$VERSION-armv7-linux-deb10.tar.xz
+              dlHash: $(sha256sum "cabal-install-$VERSION-armv7-linux-deb10.tar.xz" | awk '{ print $1 }')
+EOF
+
---
+++ scripts/release/download-gh-artifacts.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+RELEASE=$1
+VERSION=${RELEASE#cabal-install-v}
+SIGNER=$2
+
+echo "RELEASE: $RELEASE"
+echo "SIGNER: $SIGNER"
+
+for com in gh gpg curl sha256sum ; do
+	command -V ${com} >/dev/null 2>&1
+done
+
+[ ! -e "gh-release-artifacts/cabal-${VERSION}" ]
+
+mkdir -p "gh-release-artifacts/cabal-${VERSION}"
+
+git archive --format=tar.gz -o "gh-release-artifacts/cabal-${VERSION}/cabal-${VERSION}-src.tar.gz" --prefix="cabal-${VERSION}/" HEAD
+
+cd "gh-release-artifacts/cabal-${VERSION}"
+
+# github
+gh release download "$RELEASE"
+
+sha256sum ./* > SHA256SUMS
+gpg --detach-sign -u "${SIGNER}" SHA256SUMS
+
+gh release upload "$RELEASE" "cabal-${VERSION}-src.tar.gz" SHA256SUMS SHA256SUMS.sig
+
