cdd544c5c78c4bf00e4c23e365e2221a68ce3501
Andrea Bedini <andrea@andreabedini.com>
2025-04-02 14:20:57 +0800

    log solver at verbose

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -829,7 +829,7 @@ rebuildInstallPlan
                 projectConfigBuildOnly
 
             solverSettings = resolveSolverSettings projectConfig
-            logMsg message rest = debugNoWrap verbosity message >> rest
+            logMsg message rest = infoNoWrap verbosity message >> rest
 
             localPackagesEnabledStanzas =
               Map.fromList
