8581f0c30dc7e08a4439a7012d0cad88360db282
Andrea Bedini <andrea@andreabedini.com>
2025-04-23 18:00:53 +0800

    refactor: pass only relevant information

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -800,7 +800,7 @@ rebuildInstallPlan
                     planPackages
                       verbosity
                       solverSettings
-                      toolchains
+                      compilerAndPlatform
                       pkgConfigDbs
                       ipis
                       sourcePkgDb
@@ -813,6 +813,11 @@ rebuildInstallPlan
                     dieWithException verbosity $ PhaseRunSolverErr msg
                   Right plan -> return (plan, ipis, pkgConfigDbs, tis, ar)
           where
+            compilerAndPlatform =
+              fmap
+                (\Toolchain{toolchainCompiler, toolchainPlatform} -> (compilerInfo toolchainCompiler, toolchainPlatform))
+                toolchains
+
             -- corePackageDbs :: Staged PackageDBStackCWD
             -- corePackageDbs = 
             --   Cabal.interpretPackageDbFlags False <$> Staged (\case
@@ -1284,7 +1289,7 @@ getPackageSourceHashes verbosity withRepoCtx solverPlan = do
 planPackages
   :: Verbosity
   -> SolverSettings
-  -> Staged Toolchain
+  -> Staged (CompilerInfo, Platform)
   -> Staged (Maybe PkgConfigDb)
   -> Staged InstalledPackageIndex
   -> SourcePackageDb
@@ -1300,11 +1305,7 @@ planPackages
   sourcePkgs
   localPackages
   pkgStanzasEnable =
-    resolveDependencies
-      (fmap (\Toolchain{toolchainCompiler, toolchainPlatform} -> (compilerInfo toolchainCompiler, toolchainPlatform)) toolchains)
-      pkgConfigDbs
-      installedPkgs
-      resolverParams
+    resolveDependencies toolchains pkgConfigDbs installedPkgs resolverParams
     where
       -- TODO: [nice to have] disable multiple instances restriction in
       -- the solver, but then make sure we can cope with that in the
