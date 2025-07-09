4cf714dafcd2ef7b6d656f5dc04470f974e50a8e
Julian Ospald <hasufell@posteo.de>
2025-07-09 12:20:35 +0800

    Add rebase CI

---
+++ .github/scripts/rebase_local.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eux
+
+upstream_repo=$1
+# of the form [<branch>:<rebase-target>], e.g.:
+#   BA:master B1:master B2:B1 B3:B2
+spec=$2
+
+{
+
+git remote add upstream "${upstream_repo}" || true
+git fetch upstream
+
+output_branches=()
+
+# sync master with upstream
+git checkout master
+git reset --hard upstream/master
+output_branches+=( "master" )
+
+# rebase each branch
+mkdir -p rebase
+for branch_spec in ${spec} ; do
+	branch=$(echo "${branch_spec}" | awk -F ':' '{ print $1 }')
+	(
+	rebase_target=$(echo "${branch_spec}" | awk -F ':' '{ print $2 }')
+	git checkout "${branch}"
+    common_ancestor=$(git merge-base "${branch}" "origin/${rebase_target}")
+	[ -e rebase/"${branch}" ] && exit 1
+    mkdir -p rebase/"${branch}"
+	cd rebase/"${branch}"
+	echo "${common_ancestor}" > BASE_COMMIT
+    git format-patch "${common_ancestor}".."${branch}"
+	if compgen -G "*.patch" > /dev/null; then
+	  git reset --hard "${rebase_target}"
+      git am --3way *.patch
+	fi
+    )
+	output_branches+=( "${branch}" )
+done
+
+} >&2
+
+echo "${output_branches[*]}"
+
---
+++ .github/scripts/rebase_spec.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# Branch regex we consider for rebase targets.
+# For our purposes this is usually 'stable-haskell/feature/*'.
+# 'master' is always considered.
+branch_regex=$1
+shift 1
+declare -a input_branches
+input_branches=( "$@" )
+set -eux
+
+[ ${#input_branches[@]} -eq 0 ] &&
+	input_branches=( $(gh pr list --label rebase --state open --json headRefName --jq ".[] | select( .headRefName | match(\"${branch_regex}\")) | .headRefName" --template '{{range .}}{{tablerow .headRefName}}{{end}}') )
+
+branch_list=( )
+declare -A branch_map
+
+# @FUNCTION: die
+# @USAGE: [msg]
+# @DESCRIPTION:
+# Exits the shell script with status code 2
+# and prints the given message in red to STDERR, if any.
+die() {
+    (>&2 red_message "$1")
+    exit 2
+}
+
+# @FUNCTION: red_message
+# @USAGE: <msg>
+# @DESCRIPTION:
+# Print a red message.
+red_message() {
+    printf "\\033[0;31m%s\\033[0m\\n" "$1"
+}
+
+# @FUNCTION: array_contains
+# @USAGE: <arr_ref> <val>
+# @DESCRIPTION:
+# Checks whether the array reference contains the given value.
+# @RETURN: 0 if value exists, 1 otherwise
+array_contains() {
+    local -n arr=$1
+    local val=$2
+    shift 2
+    if [[ " ${arr[*]} " =~ [[:space:]]${val}[[:space:]] ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+max_backtrack=10
+
+# @FUNCTION: backtrack
+# @USAGE: <map_ref> <start_key> <abort_value>
+# @DESCRIPTION:
+# Backtrack dependencies through an array list.
+# E.g. given an associated array with key value pairs of:
+#   B1 -> M
+#   B2 -> B1
+#   B3 -> B2
+#
+# ...if we pass B3 as start_key and M as abort_value, then
+# we receive the flattened ordered list "B1 B2 B3"
+# @STDOUT: space separated list of backtracked values
+backtrack() {
+    backtrack_ 0 "$1" "$2" "$3"
+}
+
+# internal to track backtrack depth
+backtrack_() {
+    local depth=$1
+    if [[ $depth -gt $max_backtrack ]] ; then
+        die "Dependency backtracking too deep... aborting!"
+    fi
+    shift 1
+
+    if [[ $1 != map ]] ; then
+      local -n map=$1
+    fi
+
+    local base=$2
+    local abort_value=$3
+    local value
+
+    if [ "${base}" = "${abort_value}" ] ; then
+        return
+    fi
+
+    value=${map[$base]}
+
+    if [ "${value}" = "${abort_value}" ] ; then
+        if ! array_contains branch_list "${base}" ; then
+          echo "${base}"
+        fi
+    else
+        if array_contains branch_list "${base}" ; then
+          backtrack_ $((depth++)) map "${map[$value]}" "${abort_value}"
+        else
+          echo "$(backtrack_ $((depth++)) map "${map[$base]}" "${abort_value}")" "${base}"
+        fi
+    fi
+}
+
+{
+
+# create branch rebase tree
+# we're doing that on the state of the local tree/master
+while IFS= read -r branch || [[ -n $branch ]]; do
+    rebase_target=$(git branch --merged "${branch}" --sort="ahead-behind:${branch}" --format="%(refname:short)" | grep -e "${branch_regex}" -e '^master$' | awk 'NR==2{print;exit}')
+
+	# this is the case when the branch is actually behind master... we then
+	# still want to rebase against master
+	if [ -z "${rebase_target}" ] ; then
+		rebase_target=master
+	fi
+
+    branch_map["${branch}"]="${rebase_target}"
+done < <(printf '%s\n' "${input_branches[@]}")
+
+} >&2
+
+# flatten recursively
+for key in "${!branch_map[@]}"; do
+    value=${branch_map[$key]}
+    if [ "${value}" = "master" ] ; then
+        if ! array_contains branch_list "${key}" ; then
+          branch_list+=( "${key}" )
+        fi
+    else
+        # shellcheck disable=SC2207
+        branch_list+=( $(backtrack branch_map "$key" "master") )
+    fi
+done
+unset key
+
+result=( )
+for key in "${branch_list[@]}"; do
+    result+=( "${key}:${branch_map[$key]}" )
+done
+echo "${result[@]}"
+
