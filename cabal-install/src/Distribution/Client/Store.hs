a0e204b8bf30fe8e4bc5539391118309338331f4
Andrea Bedini <andrea@andreabedini.com>
2025-11-20 13:51:39 +0800

    refactor: use package.conf.d as GHC does

--- cabal-install/src/Distribution/Client/Store.hs
+++ cabal-install/src/Distribution/Client/Store.hs
@@ -135,7 +135,7 @@ getStoreEntries StoreDirLayout{storeDirectory} compiler = do
   return $! mkEntries paths
   where
     mkEntries =
-      Set.delete (mkUnitId "package.db")
+      Set.delete (mkUnitId "package.conf.d")
         . Set.delete (mkUnitId "incoming")
         . Set.fromList
         . map mkUnitId
